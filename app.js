ed94446b337c3df7fa849355e2202744eda2a730
JP <jp@servoitsolutions.ph>
2023-05-30 10:42:31 +0800

    add morgan and helmet dependencies

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 require('dotenv').config();
+//importing the dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const apiRoutes = require('./routes/api.routes');
-
-const app = express();
+// defining port of the server
 const PORT = process.env.PORT;
-
+// defining the Express app
+const app = express();
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   }),
 );
-
+// adding Helmet to enhance your API's security
+app.use(helmet());
+// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// enabling CORS for all requests
 app.use(cors());
-
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+// defining an endpoint
 app.get('/', (req, res) => {
   res.json({ message: 'ok' });
 });
-
+// declaring routes
 app.use('/api', apiRoutes);
 /* Error handler middleware */
 app.use((req, res, next) => {
@@ -40,7 +48,7 @@ app.use((err, req, res, next) => {
   });
   return;
 });
-
+// starting the server
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
